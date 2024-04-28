4f9ea4d7d95b078498e0268950dad697d8380ab9
DaVinci25 <122686008+DaVinci25@users.noreply.github.com>
2024-04-28 19:12:02 +0200

    2 comit

--- src/main/java/com/example/autoavaluacio_de_salut/HelloController.java
+++ src/main/java/com/example/autoavaluacio_de_salut/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.autoavaluacio_de_salut;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -8,7 +9,14 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void addDataAction(ActionEvent event) {
+        // Aquí puedes implementar la lógica para agregar datos
+        System.out.println("Has seleccionado Afegir dades");
+    }
+
+    @FXML
+    private void viewStatsAction(ActionEvent event) {
+        // Aquí puedes implementar la lógica para ver estadísticas
+        System.out.println("Has seleccionado Veure estadístiques");
     }
 }
