d8e02c5d0b396a49c8f4b95627f2529a0dc10b80
ArmandosLP <155646008+ArmandosLP@users.noreply.github.com>
2024-04-28 18:30:29 +0200

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.autoavaluacio_de_salut {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.autoavaluacio_de_salut to javafx.fxml;
+    exports com.example.autoavaluacio_de_salut;
+}
