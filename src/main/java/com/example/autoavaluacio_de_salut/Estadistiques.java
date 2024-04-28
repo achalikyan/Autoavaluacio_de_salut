888bacb21b1f18ac1cfd6173708e479aeae9f740
DaVinci25 <122686008+DaVinci25@users.noreply.github.com>
2024-04-28 20:34:50 +0200

    gdfgd

---
+++ src/main/java/com/example/autoavaluacio_de_salut/Estadistiques.java
@@ -0,0 +1,23 @@
+package com.example.autoavaluacio_de_salut;
+
+import javafx.event.ActionEvent;
+
+public class Estadistiques {
+    public void showDaysCount(ActionEvent actionEvent) {
+    }
+
+    public void showAverageMood(ActionEvent actionEvent) {
+    }
+
+    public void showAveragePhysicalState(ActionEvent actionEvent) {
+    }
+
+    public void showMostConsumedFoodOrDrink(ActionEvent actionEvent) {
+    }
+
+    public void showBestMoodDays(ActionEvent actionEvent) {
+    }
+
+    public void showBestPhysicalStateDays(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/autoavaluacio_de_salut/HelloController.java
+++ src/main/java/com/example/autoavaluacio_de_salut/HelloController.java
@@ -1,22 +1,43 @@
 package com.example.autoavaluacio_de_salut;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    private void addDataAction(ActionEvent event) {
-        // Aquí puedes implementar la lógica para agregar datos
-        System.out.println("Has seleccionado Afegir dades");
+    private Button Agregar;
+
+    @FXML
+    private void Agregar(ActionEvent event) {
+        // Cargar la escena de Agregar.fxml
+        loadFXMLScene("Agregar.fxml");
     }
 
     @FXML
     private void viewStatsAction(ActionEvent event) {
         // Aquí puedes implementar la lógica para ver estadísticas
-        System.out.println("Has seleccionado Veure estadístiques");
+        loadFXMLScene("Estadistiques.fxml");
+    }
+
+    private void loadFXMLScene(String fxmlFileName) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFileName));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
