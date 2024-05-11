882b41fc5f6ebb97b735aaaf11c39118e0316a7f
ArmandosLP <155646008+ArmandosLP@users.noreply.github.com>
2024-05-11 22:13:22 +0200

    Lector agregado

--- src/main/java/com/example/autoavaluacio_de_salut/EstadisticasController.java
+++ src/main/java/com/example/autoavaluacio_de_salut/EstadisticasController.java
@@ -4,6 +4,12 @@ package com.example.autoavaluacio_de_salut;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
 public class EstadisticasController {
     @FXML
     private Label cantidadDiasLabel;
@@ -24,6 +30,8 @@ public class EstadisticasController {
     private Label diasMejorEstadoFisicoLabel;
 
     private Estadistiques estadisticas; // Suponiendo que tengas un objeto Estadisticas
+    ArrayList<Dato> datos = new ArrayList<Dato>();
+
 
     // Método para inicializar los datos de las estadísticas
     public void initData(Estadistiques estadisticas) {
@@ -39,5 +47,27 @@ public class EstadisticasController {
         comidaBebidaMasConsumidaLabel.setText("Menjar o beguda més consumida: " + estadisticas.getComidaBebidaMasConsumida());
         diasMejorEstadoAnimoLabel.setText("Dies amb millor estat d'ànim: " + estadisticas.getDiasMejorEstadoAnimo());
         diasMejorEstadoFisicoLabel.setText("Dies amb millor estat físic: " + estadisticas.getDiasMejorEstadoFisico());
+    }
+
+
+
+
+    public void cargarObjetos() {
+        try (FileInputStream fileIn = new FileInputStream(".//datos.txt");
+             ObjectInputStream objectIn = new ObjectInputStream(fileIn)) {
+            Object objeto;
+            while ((objeto = objectIn.readObject()) != null) {
+                // Verificar si el objeto es de tipo Dato
+                if (objeto instanceof Dato) {
+                    // Procesar el objeto Dato leído
+                    Dato dato = (Dato) objeto;
+                    datos.add(dato);
+                }
+            }
+        } catch (EOFException e) {
+            // Fin del archivo
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
