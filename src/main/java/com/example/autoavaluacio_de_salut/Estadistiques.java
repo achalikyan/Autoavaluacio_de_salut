767ac0d89586d4ced04921300079d345994d080a
DaVinci25 <122686008+DaVinci25@users.noreply.github.com>
2024-05-11 22:07:21 +0200

    dadasdsd

--- src/main/java/com/example/autoavaluacio_de_salut/Estadistiques.java
+++ src/main/java/com/example/autoavaluacio_de_salut/Estadistiques.java
@@ -3,21 +3,67 @@ package com.example.autoavaluacio_de_salut;
 import javafx.event.ActionEvent;
 
 public class Estadistiques {
-    public void showDaysCount(ActionEvent actionEvent) {
+    private int cantidadDias;
+    private double mediaEstadoAnimo;
+    private double mediaEstadoFisico;
+    private String comidaBebidaMasConsumida;
+    private int diasMejorEstadoAnimo;
+    private int diasMejorEstadoFisico;
+
+    public Estadistiques(int cantidadDias, double mediaEstadoAnimo, double mediaEstadoFisico, String comidaBebidaMasConsumida, int diasMejorEstadoAnimo, int diasMejorEstadoFisico) {
+        this.cantidadDias = cantidadDias;
+        this.mediaEstadoAnimo = mediaEstadoAnimo;
+        this.mediaEstadoFisico = mediaEstadoFisico;
+        this.comidaBebidaMasConsumida = comidaBebidaMasConsumida;
+        this.diasMejorEstadoAnimo = diasMejorEstadoAnimo;
+        this.diasMejorEstadoFisico = diasMejorEstadoFisico;
     }
 
-    public void showAverageMood(ActionEvent actionEvent) {
+    public int getCantidadDias() {
+        return cantidadDias;
     }
 
-    public void showAveragePhysicalState(ActionEvent actionEvent) {
+    public void setCantidadDias(int cantidadDias) {
+        this.cantidadDias = cantidadDias;
     }
 
-    public void showMostConsumedFoodOrDrink(ActionEvent actionEvent) {
+    public double getMediaEstadoAnimo() {
+        return mediaEstadoAnimo;
     }
 
-    public void showBestMoodDays(ActionEvent actionEvent) {
+    public void setMediaEstadoAnimo(double mediaEstadoAnimo) {
+        this.mediaEstadoAnimo = mediaEstadoAnimo;
     }
 
-    public void showBestPhysicalStateDays(ActionEvent actionEvent) {
+    public double getMediaEstadoFisico() {
+        return mediaEstadoFisico;
+    }
+
+    public void setMediaEstadoFisico(double mediaEstadoFisico) {
+        this.mediaEstadoFisico = mediaEstadoFisico;
+    }
+
+    public String getComidaBebidaMasConsumida() {
+        return comidaBebidaMasConsumida;
+    }
+
+    public void setComidaBebidaMasConsumida(String comidaBebidaMasConsumida) {
+        this.comidaBebidaMasConsumida = comidaBebidaMasConsumida;
+    }
+
+    public int getDiasMejorEstadoAnimo() {
+        return diasMejorEstadoAnimo;
+    }
+
+    public void setDiasMejorEstadoAnimo(int diasMejorEstadoAnimo) {
+        this.diasMejorEstadoAnimo = diasMejorEstadoAnimo;
+    }
+
+    public int getDiasMejorEstadoFisico() {
+        return diasMejorEstadoFisico;
+    }
+
+    public void setDiasMejorEstadoFisico(int diasMejorEstadoFisico) {
+        this.diasMejorEstadoFisico = diasMejorEstadoFisico;
     }
 }
