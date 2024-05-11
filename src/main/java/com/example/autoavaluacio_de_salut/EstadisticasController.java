32a30475f325037b1aba12a02b226ec36979454c
DaVinci25 <122686008+DaVinci25@users.noreply.github.com>
2024-05-11 22:40:19 +0200

    Estadistiques

--- src/main/java/com/example/autoavaluacio_de_salut/EstadisticasController.java
+++ src/main/java/com/example/autoavaluacio_de_salut/EstadisticasController.java
@@ -29,24 +29,18 @@ public class EstadisticasController {
     @FXML
     private Label diasMejorEstadoFisicoLabel;
 
-    private Estadistiques estadisticas; // Suponiendo que tengas un objeto Estadisticas
+    private Estadistiques estadisticas = new Estadistiques(); // Suponiendo que tengas un objeto Estadisticas
 
 
-
-    // Método para inicializar los datos de las estadísticas
-    public void initData(Estadistiques estadisticas) {
-        this.estadisticas = estadisticas;
-        actualizarEtiquetas();
-    }
 
     // Método para actualizar el texto de las etiquetas con los datos de estadísticas
     private void actualizarEtiquetas() {
         cantidadDiasLabel.setText("Quantitat de dies introduïts: " + estadisticas.getCantidadDias());
-        mediaEstadoAnimoLabel.setText("Mitjana de l'estat d'ànim: " + estadisticas.getMediaEstadoAnimo());
+      /*  mediaEstadoAnimoLabel.setText("Mitjana de l'estat d'ànim: " + estadisticas.getMediaEstadoAnimo());
         mediaEstadoFisicoLabel.setText("Mitjana de l'estat físic: " + estadisticas.getMediaEstadoFisico());
         comidaBebidaMasConsumidaLabel.setText("Menjar o beguda més consumida: " + estadisticas.getComidaBebidaMasConsumida());
         diasMejorEstadoAnimoLabel.setText("Dies amb millor estat d'ànim: " + estadisticas.getDiasMejorEstadoAnimo());
-        diasMejorEstadoFisicoLabel.setText("Dies amb millor estat físic: " + estadisticas.getDiasMejorEstadoFisico());
+        diasMejorEstadoFisicoLabel.setText("Dies amb millor estat físic: " + estadisticas.getDiasMejorEstadoFisico());*/
     }
 
 }
